15ad03077dd05af17b09b6608965c111f50fcfda
IasmimFrancoooo <00001094942947sp@al.educacao.sp.gov.br>
2024-11-24 22:49:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ed. Fisica',
+    'O que é volei?',
+    'Um esporte com bola'
+)
+
+criaCartao(
+    'Geografia',
+    'Onde fica o mexico?',
+    'Na America do Norte'
+)
+
+criaCartao(
+    'Matematica',
+    '11+4=?',
+    '15'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz olhos em Inglês?',
+    'Olhos em ingles é Eyes'
+)
